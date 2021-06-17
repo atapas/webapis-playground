cd415d3d76932541aa78e5ab0e982ba765f57fdb
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-06-17 11:37:10 +0530

    fix: Style fixes battery API

--- src/battery-status/index.js
+++ src/battery-status/index.js
@@ -6,8 +6,6 @@ if (navigator.getBattery) {
       function updateAllBatteryInfo() {
         updateLevelInfo()
         updateChargeInfo()
-        updateChargingInfo()
-        updateDischargingInfo()
       }
       updateAllBatteryInfo()
 
@@ -41,45 +39,21 @@ if (navigator.getBattery) {
         level = battery.level * 100;
         document.getElementById('battery-status-level').innerHTML = `<b style="color: ${getColor()}">${level}</b>%`;
       }
-
-      battery.addEventListener("chargingtimechange", function () {
-        updateChargingInfo()
-      })
-
-      function updateChargingInfo() {
-        console.log(
-          "Battery charging time: " + battery.chargingTime + " seconds"
-        )
-        chargingTime = battery.chargingTime;
-        document.getElementById('battery-status-charge-time').innerHTML = `<b>${chargingTime}</b> seconds`;
-      }
-
-      battery.addEventListener("dischargingtimechange", function () {
-        updateDischargingInfo()
-      })
-
-      function updateDischargingInfo() {
-        console.log(
-          "Battery discharging time: " + battery.dischargingTime + " seconds"
-        )
-        dischargingTime = battery.dischargingTime;
-        document.getElementById('battery-status-discharge-time').innerHTML = `<b>${dischargingTime}</b> seconds`;
-      }
     })
   } else {
     console.log(`This API is Not Supported`);
   }
 
-  const getColor = () => {
-    let color = "#FFFFFF"
+  function getColor() {
+    let color = "#000000";
 
     if (level >= 0 && level <= 20) {
-      color = "#FF0000"
+      color = "#FF0000";
     } else if (level > 20 && level <= 60) {
-      color = "#FFBF00"
+      color = "#FFBF00";
     }
     if (level > 60 && level <= 100) {
-      color = "#008000"
+      color = "#008000";
     }
-    return color
+    return color;
   }
