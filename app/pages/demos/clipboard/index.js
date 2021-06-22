8a50b494360938a4dcdae6a2cc6810cca9155399
Tapas Adhikary <tapas.adhikary@gmail.com>
2021-06-22 20:10:52 +0530

    feat: Migrated the clipboard demo

---
+++ app/pages/demos/clipboard/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+// apis
+import { performCopy, performPaste } from './api';
+
+const Clipboard = () => {
+
+    return(
+
+    <div className="demos">
+        <form>
+            <label htmlFor="copy-input">Copy</label>
+            <input 
+                id="copy-input" 
+                type="text" 
+                name="copy" 
+                placeholder="Write Something..."/>
+            <button onClick={performCopy}>Copy</button>
+        </form>
+
+        <form>
+            <label htmlFor="paste">Paste</label>
+            <input
+                id="paste-input"  
+                type="text" 
+                name="paste"
+                readOnly />
+            <button onClick={performPaste}>Paste</button>
+        </form>
+    </div>
+    )
+};
+
+export default Clipboard;
