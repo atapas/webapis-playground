ccdcf50ee495026241f2d5879db204eeb6855c31
S Nitesh Singh <niteshseram@gmail.com>
2021-09-29 18:19:08 +0530

    fix: :wheelchair: improve accessibility score

--- components/core/Header.js
+++ components/core/Header.js
@@ -14,25 +14,28 @@ const Header = ({ showSearch }) => {
         </a>
       </Link>
       <ul className="links">
-        <a
-          className="github"
-          title="Source Code"
-          href="https://github.com/atapas/webapis-playground"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FiGithub size={32} color="#FFFFFF" />
-        </a>
-
-        <a
-          className="tweet"
-          title="Tweet About it"
-          href="https://twitter.com/intent/tweet?text=Check%20this%20amazing%20Web%20APIs%20demo%20https://webapis-playground.vercel.app/,%20created%20by%20@tapasadhikary%20@MaxProgramming1%0A%0A%23DEVCommunity%20%23100DaysOfCode"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FiTwitter size={32} color="#FFFFFF" />
-        </a>
+        <li>
+          <a
+            className="github"
+            title="Source Code"
+            href="https://github.com/atapas/webapis-playground"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiGithub size={32} color="#FFFFFF" />
+          </a>
+        </li>
+        <li>
+          <a
+            className="tweet"
+            title="Tweet About it"
+            href="https://twitter.com/intent/tweet?text=Check%20this%20amazing%20Web%20APIs%20demo%20https://webapis-playground.vercel.app/,%20created%20by%20@tapasadhikary%20@MaxProgramming1%0A%0A%23DEVCommunity%20%23100DaysOfCode"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiTwitter size={32} color="#FFFFFF" />
+          </a>
+        </li>
       </ul>
     </header>
   );
