f6252dc6bb0710142fa4a6157a1a3715a2fbb918
omri206attiya@gmail.com <dontthinkicandoit>
2021-10-01 13:35:18 +0300

    use SearchBox component in App.js and add simple search function

--- components/core/App.js
+++ components/core/App.js
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 // import data from json
 import { getAllDemos } from 'utils/data/data-access';
 
 // Demo component
 import { Demo } from '..';
+import { SearchBox } from './search-box/SearchBox';
 
 const App = () => {
   const [demos, setDemos] = useState([]);
@@ -17,11 +18,25 @@ const App = () => {
     setLoading(false);
   }, []);
 
+  const searchAPI = (searchText) => {
+    const lowerSearchText = searchText.toLowerCase();
+    setLoading(true);
+    let searchedDemos = getAllDemos();
+    if (searchText) {
+      searchedDemos = searchedDemos
+        .filter(demo => demo.title?.toLowerCase().includes(lowerSearchText));
+    }
+
+    setDemos(searchedDemos);
+    setLoading(false);
+  };
+
   return (
     <>
+      <SearchBox onSearch={searchAPI} />
       <div className="demo-list">
         {(!loading && demos.length) > 0 &&
-          demos.map((demo, index) => <Demo demo={demo} key={index} />)}
+        demos.map((demo, index) => <Demo demo={demo} key={index} />)}
       </div>
     </>
   );
