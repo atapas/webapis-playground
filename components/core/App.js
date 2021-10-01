a15c6adf80548c090de10310ebdf8e1b3ff654d5
tiger-yash <tigeryashraj@gmail.com>
2021-10-01 21:52:01 +0530

    revert

--- components/core/App.js
+++ components/core/App.js
@@ -5,36 +5,23 @@ import { getAllDemos } from 'utils/data/data-access';
 
 // Demo component
 import { Demo } from '..';
-import { SearchBox } from './SearchBox';
-
-const NOT_FOUND_TEXT = 'No matched demo found';
 
 const App = () => {
   const [demos, setDemos] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const searchAPI = (searchText) => {
-    const lowerSearchText = searchText.toLowerCase();
-    setLoading(true);
-    let searchedDemos = getAllDemos();
-    if (searchText) {
-      searchedDemos = searchedDemos
-        .filter(demo => demo.title?.toLowerCase().includes(lowerSearchText));
-    }
-
-    setDemos(searchedDemos);
+  useEffect(() => {
+    const allDemos = getAllDemos();
+    console.log({ allDemos });
+    setDemos(allDemos);
     setLoading(false);
-  };
+  }, []);
 
   return (
     <>
-      <SearchBox onSearch={searchAPI} />
       <div className="tw-flex tw-flex-wrap tw-items-center tw-justify-center tw-p-10px">
-        {(!loading && demos.length > 0) ? (
-          demos.map((demo, index) => <Demo demo={demo} key={index} />)
-        ) : (
-          <div className="not-found">{NOT_FOUND_TEXT}</div>
-        )}
+        {(!loading && demos.length) > 0 &&
+          demos.map((demo, index) => <Demo demo={demo} key={index} />)}
       </div>
     </>
   );
