f7b777a48af7d66ee9292d65feceb40a47a3a43d
aanchal <fatwaniaanchal@gmail.com>
2021-10-02 22:01:57 +0530

    fix(git link): Removed inline styles and modified placement

--- components/core/GithubCodeLink.js
+++ components/core/GithubCodeLink.js
@@ -4,21 +4,8 @@ import { FiGithub } from 'react-icons/fi';
 const GithubCodeLink = ({ url }) => {
   return (
     <div className="flex-colums">
-      <a
-        target="_blank"
-        rel="noreferrer"
-        href={url}
-        style={{
-          margin: '50px',
-        }}
-      >
-        <button
-          style={{
-            backgroundColor: '#000000',
-            color: '#FFFFFF',
-            float: 'right',
-          }}
-        >
+      <a target="_blank" rel="noreferrer" href={url}>
+        <button className="git-link">
           See Code <FiGithub />
         </button>
       </a>
