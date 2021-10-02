a23a53fba9b9b1c413175f3362e4aaeb2e005538
Brandon bishop <brandon.lw.bishop.1999@gmail.com>
2021-10-02 15:10:44 +0100

    Added a Alphabetial Sort Function to getAllDemos()

--- utils/data/data-access.js
+++ utils/data/data-access.js
@@ -1,6 +1,17 @@
 import DEMOS from './demos';
 
 const getAllDemos = () => {
+  //sort demos Alphabetically
+  DEMOS.sort(function (a, b) {
+    if (a.id < b.id) {
+      return -1;
+    }
+    if (a.id > b.id) {
+      return 1;
+    }
+    return 0;
+  });
+
   return DEMOS;
 };
 
