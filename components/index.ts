3527c406836fe82a467dcb27ddcf4d443678212e
Matheus Verissimo <mverissimoo@hotmail.com>
2021-10-18 10:56:11 -0300

    fix(app): remove unsed imports

--- components/index.ts
+++ components/index.ts
@@ -22,11 +22,9 @@ export { InputSearch } from './InputSearch';
 export type { InputSearchProps } from './InputSearch';
 
 // core
-export { default as App } from './core/App';
 export { default as GithubCodeLink } from './core/GithubCodeLink';
 
 // util
-export { default as Demo } from './util/Demo';
 export { default as DemoInfo } from './util/DemoInfo';
 export { default as Creator } from './util/Creator';
 export { default as NotSupported } from './util/NotSupported';
