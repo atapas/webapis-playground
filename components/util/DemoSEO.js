fd77df3b29d7f2e7fde6862714fd217dddb41e5c
jiyun99 <rrwldbs@gmail.com>
2021-10-19 03:46:56 -0400

    seo update

--- components/util/DemoSEO.js
+++ components/util/DemoSEO.js
@@ -3,7 +3,7 @@ import SEO from '@bradgarropy/next-seo';
 const DemoSEO = ({ title, description }) => {
   return (
     <div>
-      <img src="https://webapis-playground.vercel.app/readme/og.png" />
+      <img src=".\public\readme\og.png" />
       <SEO title={title} description={description} />
     </div>
   );
