12e658e1bad003ce74fea82d658d57db170e5622
Koustov <koustov.maitra@microfocus.com>
2021-10-21 17:53:10 +0530

    ui update

--- components/Demo/Demo.tsx
+++ components/Demo/Demo.tsx
@@ -1,8 +1,5 @@
-import Link from 'next/link';
-
-import { FaBolt } from 'react-icons/fa';
-
-import { Button } from 'components';
+import { FiTwitter } from 'react-icons/fi';
+import { IconButton } from 'components';
 
 /**
  *
@@ -15,57 +12,113 @@ export interface DemoProps {
 
 function Demo({ data }: DemoProps) {
   return (
-    <article
+    <div
       className="
-        tw-flex
+      card-group
+      tw-flex
         tw-flex-col
-        tw-items-center
-        tw-py-6
-        tw-px-6
-        tw-text-center
-        tw-border
-        tw-border-solid
-        tw-border-gray-200
-        tw-rounded-lg
-        tw-bg-white
+      tw-h-card
+      tw-bg-base
+      tw-rounded-2xl
+      tw-border-white
+      tw-border-8
+      tw-shadow-sm
+      hover:tw-transform hover:tw-scale-105
+      tw-transition-all
+       tw-cursor-pointer
+       tw-overflow-hidden
       "
     >
       <div
         className="
-          tw-text-4xl
-          tw-mb-4
-        "
+       tw-h-48
+       tw-m-full
+       tw-bg-red-700
+       tw-rounded-header
+       tw-grid
+       tw-bg-header
+       tw-bg-gradient-to-bl
+       tw-from-bgstart
+       tw-to-bgend
+       card-group-hover:tw-h-0
+      "
       >
-        {data.emoji}
+        <div
+          className="
+          tw-text-7xl
+          tw-text-white
+          tw-flex
+          tw-justify-center
+          tw-items-center
+        "
+        >
+          {data.icon}
+          {/* {data.emoji} */}
+        </div>
       </div>
-
-      <h1
+      <div
         className="
-          tw-mb-2
-          tw-text-xl
-          tw-font-bold
-        "
+        tw-flex
+        tw-flex-col
+        tw-flex-1
+        tw-items-center
+        tw-mx-auto
+        tw-my-4
+      "
       >
-        {data.title}
-      </h1>
-
-      <p
-        className="
-          tw-mb-8
-          tw-text-sm
+        <div
+          className="
+        tw-text-center
+        tw-text-lg
+        tw-mb-8
+        "
+        >
+          {data.title}
+        </div>
+        <div
+          className="
+        tw-text-center
+        tw-text-md
+        tw-mb-8
+        tw-text-sm
           tw-text-gray-600
           tw-line-clamp-3
         "
+        >
+          {data.desc}
+        </div>
+      </div>
+      <div
+        className="
+      tw-flex
+      tw-h-12
+      tw-border-t-2	
+      "
       >
-        {data.desc}
-      </p>
+        <div>
+          <IconButton
+            as="a"
+            href={`https://twitter.com/${data.creator.twitter}`}
+            rel="noopener noreferrer"
+            target="_blank"
+            ariaLabel="Share on twitter"
+            className="tw-flex-1 tw-font-thin"
+            icon={<>{data.creator.name}</>}
+          />
+        </div>
 
-      <Link href={`/demos/${data.path}`} passHref>
-        <Button as="a" leftIcon={<FaBolt />}>
-          Try it
-        </Button>
-      </Link>
-    </article>
+        <div>
+          <IconButton
+            as="a"
+            href={`https://twitter.com/${data.creator.twitter}`}
+            rel="noopener noreferrer"
+            target="_blank"
+            ariaLabel="Share on twitter"
+            icon={<FiTwitter />}
+          />
+        </div>
+      </div>
+    </div>
   );
 }
 
