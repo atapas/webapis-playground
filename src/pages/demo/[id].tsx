af45995bd9c1060a77db29bdda1558788e64ef61
Matheus Verissimo <mverissimoo@hotmail.com>
2021-10-22 17:32:53 -0300

    feat(page): add dynamic demo

---
+++ src/pages/demo/[id].tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+
+import { getDemoById } from 'services/demo';
+
+function Demo() {
+  let router = useRouter();
+  let id = router.query.id as string;
+
+  let [demo, setdemo] = React.useState(getDemoById(id));
+
+  React.useEffect(() => {
+    setdemo(getDemoById(id));
+  }, [id]);
+
+  let Component = dynamic(() => import(`../../modules/demos/${id}`), {
+    ssr: false,
+  });
+
+  return <Component />;
+}
+
+export default Demo;
