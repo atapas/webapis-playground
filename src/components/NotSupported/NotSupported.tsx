ce490833a8ceb4a117105b8fa1b75a16940cfe56
Matheus Verissimo <mverissimoo@hotmail.com>
2021-10-30 19:59:04 -0300

    style(fullscreen): remove center alignment

---
+++ src/components/NotSupported/NotSupported.tsx
@@ -0,0 +1,5 @@
+function NotSupported() {
+  return <span> It seems your browser does not support this feature.</span>;
+}
+
+export default NotSupported;
--- src/modules/demos/fullscreen/index.tsx
+++ src/modules/demos/fullscreen/index.tsx
@@ -1,29 +1,31 @@
-import run, { hasSupport } from '../../apis/fullscreen';
+import React from 'react';
 
 import { FaExpand } from 'react-icons/fa';
 
-import { Button } from 'components';
+import { Button, NotSupported } from 'components';
+
+import run, { hasSupport } from '../../apis/fullscreen';
 
 function FullScreen() {
   if (!hasSupport) {
-    return <h1>Unsupported</h1>;
+    return <NotSupported canIUseURL="" />;
   }
 
   return (
-    <div className="tw-flex tw-flex-col tw-items-center tw-justify-center">
+    <React.Fragment>
       <div id="js-api--fullscreen" className="tw-mb-4">
         <img
           src="https://res.cloudinary.com/atapas/image/upload/v1602226996/artists/Rock_oz3kq1.png"
-          alt="Rock"
+          alt="The Rock"
           width="200px"
           height="200px"
         />
       </div>
 
-      <Button onClick={run} leftIcon={<FaExpand />}>
-        Go to fullscreen
+      <Button leftIcon={<FaExpand />} onClick={run}>
+        Go Fullscreen
       </Button>
-    </div>
+    </React.Fragment>
   );
 }
 
