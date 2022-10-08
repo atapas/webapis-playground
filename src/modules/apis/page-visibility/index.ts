07b7c64e8fd8aabb58014d962064cb31ccd04ee3
Williams Samuel <samwill300@gmail.com>
2022-10-08 10:51:00 +0100

    fixed typescript issue on page visibility index

--- src/modules/apis/page-visibility/index.ts
+++ src/modules/apis/page-visibility/index.ts
@@ -1,15 +1,20 @@
 export let hidden: string = '';
 export let visibilityChange: string = '';
 
+interface BroswerPrefix extends Document {
+  mozHidden: any;
+  webkitHidden: any;
+}
+
 // Set the name of the "hidden" property and the change event for visibility
 if (typeof document.hidden !== 'undefined') {
   hidden = 'hidden';
   visibilityChange = 'visibilitychange';
-} else if (typeof document.mozHidden !== 'undefined') {
+} else if (typeof (<BroswerPrefix>document).mozHidden !== 'undefined') {
   // Firefox up to v17
   hidden = 'mozHidden';
   visibilityChange = 'mozvisibilitychange';
-} else if (typeof document.webkitHidden !== 'undefined') {
+} else if (typeof (<BroswerPrefix>document).webkitHidden !== 'undefined') {
   // Chrome up to v32, Android up to v4.4, Blackberry up to v10
   visibilityChange = 'webkitvisibilitychange';
   hidden = 'webkitHidden';
